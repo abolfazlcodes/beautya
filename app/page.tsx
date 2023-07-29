5b0c7047864dde754c2b290aa67f59b65ebb5aba
Abolfazl <abolfazljamshididev@gmail.com>
2023-07-29 18:26:08 +0330

    changing basic styles of the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-  );
-}
+const Home = () => {
+  return <main className="min-h-screen"></main>;
+};
+
+export default Home;
